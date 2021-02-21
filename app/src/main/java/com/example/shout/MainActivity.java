962b980be34689fbb622df61943298fc279ecab8
Piyush Goel <piyushgoel2008@gmail.com>
2021-02-21 10:31:08 +0530

    Drawer working without icons

--- app/src/main/java/com/example/shout/MainActivity.java
+++ app/src/main/java/com/example/shout/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.shout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton connect;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connect=findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+
+
+    }
